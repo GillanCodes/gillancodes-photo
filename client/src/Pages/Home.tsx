36b4a9ac39a78a2b66a1c1a5bb9ddcc3371583da
gillancodes <gillancodes@laposte.net>
2024-10-09 14:45:03 +0200

    feat: home page done with style

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
-
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="container">
+      
+      <div className="columns">
+        <div className="column">
+          <h1 id="main-title">Nicholas - Photographer</h1>
+          <p className="paragraph">Cupcake ipsum dolor sit amet danish chocolate sweet roll brownie. Tootsie roll jelly beans croissant cookie tootsie roll apple pie lemon drops. Candy canes tart brownie I love apple pie gummies soufflé. Cake apple pie croissant pie sweet. Powder marshmallow cake oat cake bonbon dessert macaroon. I love toffee biscuit I love soufflé sweet danish I love. Marzipan chocolate cake candy muffin cheesecake dessert macaroon shortbread bonbon. Cupcake pudding cake I love sesame snaps. Apple pie danish chocolate cake pudding I love caramels dragée cookie. Wafer chocolate cake I love soufflé jelly-o jelly beans. Sweet cotton candy biscuit marshmallow marshmallow powder. Sweet marshmallow oat cake jujubes donut marzipan pie.</p>
+          
+          <div id="links">
+            <p>A</p>
+            <p>A</p>
+            <p>A</p>
+            <p>A</p>
+            <p>A</p>
+          </div>
+
+          <div id="main-pic">
+            <div></div>
+          </div>
+        </div>
+      </div>
+ 
+      <div className="columns">
+        <div className="column">
+          <h2 className="subtitle">Services</h2>
+        </div>
+      </div>
+      
+      <div className="boxes">
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+
+      </div>
+
+
+
+    </div>
   )
 }
